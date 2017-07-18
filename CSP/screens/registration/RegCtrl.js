b9ca35b49277bdf0467663ddfbebc3d3490bcb81
Mila <ludochka-alsadaeva@mail.ru>
2017-07-18 14:27:48 +0300

    Сделана отмена в регистрации

--- CSP/screens/registration/RegCtrl.js
+++ CSP/screens/registration/RegCtrl.js
@@ -1,7 +1,24 @@
-RegModule.controller('RegCtrl', function(){
-	
-	
-	
-	
-	
-	});
+mainModule.controller('RegCtrl', function($scope, $http, $location){
+	$scope.user = {
+		name: "",
+		years: "",
+		city: "",
+		phone: "",
+		email: "",
+		login: "",
+		password: ""
+	};
+
+	$scope.save = function() {
+		$http.post("broker/volunteer/", $scope.user).then(
+			function(data){
+				alert("OK");
+			}, 
+			function(data){
+				alert(data);
+			}
+		);	
+	};
+	$scope.cancel = function() {$location.url('/main')}
+
+});

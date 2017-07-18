f04ec8095f707030266248cb5dc3899b95e36fdd
Mila <ludochka-alsadaeva@mail.ru>
2017-07-18 10:41:46 +0300

    Регистрация

--- CSP/js/app.js
+++ CSP/js/app.js
@@ -2,14 +2,15 @@
 
 
 var mainModule = angular.module('mainModule', ['ngRoute']); 
+var regModule = angular.module('regModule', ['ngRoute']); 
 
 mainModule.config(['$routeProvider', function($routeProvider) {
 	
 	
     //$routeProvider.when('/faculty', {templateUrl: 'partials/faculty.csp',  controller: 'FacultyCtrl'});
     $routeProvider.when('/main', {templateUrl: 'screens/main/main.csp',  controller: 'MainCtrl'});
-    
-    
+    $routeProvider.when('/registration', {templateUrl: 'screens/registration/registration.html',  controller: 'RegCtrl'});
+        
     $routeProvider.otherwise({redirectTo: '/main'});
   }]);
  

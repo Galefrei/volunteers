9198c72e9d537556786d878fa7dafa4d7b84846a
elizzaigraeva <eliz.zaigraeva@yandex.ru>
2017-07-19 15:37:30 +0700

    обновлена регистрация

--- CSP/directives/header/header.js
+++ CSP/directives/header/header.js
@@ -2,9 +2,18 @@ mainModule.directive('header', function(){
 	return{		
 		restrict:"E",					//ограничение что можно только использовать как тег <header>
 		templateUrl: 'directives/header/header.csp', 
-		controller: function() {
+		controller: function($scope) {
+			$scope.user = {};			
+			$scope.login = function() 
+			{
+				$http.post("broker/login", $scope.user).then(
+					function(data){
+						console.log(dats);
+					},
+					function(data){
+						alert(data);
+					})
+			}
 		}
 	}
-	
-	
 });
